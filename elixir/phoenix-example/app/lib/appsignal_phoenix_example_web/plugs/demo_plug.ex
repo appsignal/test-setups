388fa2f03363c5aeed0993868425432842edb3f5
Shairyar Baig <shairyarbaig@gmail.com>
2020-12-30 18:18:57 +0500

    Set sample data on root span for the middleware

--- elixir/phoenix-example/app/lib/appsignal_phoenix_example_web/plugs/demo_plug.ex
+++ elixir/phoenix-example/app/lib/appsignal_phoenix_example_web/plugs/demo_plug.ex
@@ -11,15 +11,16 @@ defmodule DemoPlug do
 
     Appsignal.instrument(fn span ->
       span
-      |> Appsignal.Span.set_namespace("graphql")
-      |> Appsignal.Span.set_name("Locales.Instrument")
-      |> Appsignal.Span.set_attribute("appsignal:category", "locale")
-      |> Appsignal.Span.set_sample_data("resolution", %{
+      |> Appsignal.Span.set_namespace("graphql") # at the moment i am not sure what this does
+      |> Appsignal.Span.set_attribute("appsignal:category", "locale") # creates a sub event in event timeline
+      |> Appsignal.Span.set_name("Locales.Instrument") # Events heading
+
+      # Sample data can only be set on the root span and not on the child span
+      Appsignal.Span.set_sample_data(Appsignal.Tracer.root_span, "custom_data", %{resolution: %{
         "current_user_permissions" => "resolution.context.current_user_permissions",
         "raw_query" => "resolution.context.conn.params",
         "graphql_args" => "inspect"
-      })
-
+      }})
     end)
     assign(conn, :locale, loc)
   end
