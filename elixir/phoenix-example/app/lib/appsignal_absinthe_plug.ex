44a7c827a5d910eb4a6ee183356f1914b6c2cdae
Shairyar Baig <shairyarbaig@gmail.com>
2021-01-08 13:04:05 +0500

    Add custom instrumentation for AbsinthePlug

--- elixir/phoenix-example/app/lib/appsignal_absinthe_plug.ex
+++ elixir/phoenix-example/app/lib/appsignal_absinthe_plug.ex
@@ -2,14 +2,17 @@ defmodule AppsignalAbsinthePlug do
   alias Appsignal.Transaction
 
   def init(_), do: nil
-    
+
   @path "/api" # Change me to your route's path
 
   def call(%Plug.Conn{request_path: @path, method: "POST"} = conn, _) do
-    Appsignal.Plug.put_name(conn, "POST " <> @path)
+    Appsignal.instrument "AppsignalAbsinthePlug", fn ->
+      Appsignal.Plug.put_name(conn, "POST " <> @path)
+      conn
+    end
   end
 
   def call(conn, _) do
     conn
   end
-end 
+end
