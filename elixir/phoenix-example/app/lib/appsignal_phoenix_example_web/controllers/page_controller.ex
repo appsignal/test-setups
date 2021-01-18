da84b1bf9d125adc4ee9b7163b935fe7cd0a2ba2
Shairyar Baig <shairyarbaig@gmail.com>
2021-01-18 18:22:57 +0500

    Logging current span

--- elixir/phoenix-example/app/lib/appsignal_phoenix_example_web/controllers/page_controller.ex
+++ elixir/phoenix-example/app/lib/appsignal_phoenix_example_web/controllers/page_controller.ex
@@ -3,11 +3,68 @@ defmodule AppsignalPhoenixExampleWeb.PageController do
 
   alias AppsignalPhoenixExample.Accounts
   alias AppsignalPhoenixExample.Accounts.User
-
+  require Logger
   plug DemoPlug, "en"
 
   def index(conn, _params) do
     users = Accounts.list_users()
+    slow()
     render(conn, "index.html", users: users)
+  end
+
+  defp slow do
+    Appsignal.instrument("slow", fn ->
+      :timer.sleep(1000)
+      Logger.info("CURRENT_SPAN_12")
+      Logger.info( Appsignal.Tracer.current_span())
+
+      Logger.info("CURRENT_SPAN_13")
+      Logger.info( Appsignal.Tracer.current_span())
+
+      Logger.info("CURRENT_SPAN_14")
+      Logger.info( Appsignal.Tracer.current_span())
+
+      Logger.info("CURRENT_SPAN_15")
+      Logger.info( Appsignal.Tracer.current_span())
+
+      Logger.info("CURRENT_SPAN_16")
+      Logger.info( Appsignal.Tracer.current_span())
+    end)
+
+    Appsignal.instrument("slow_2", fn ->
+      :timer.sleep(1000)
+      Logger.info("CURRENT_SPAN_22")
+      Logger.info( Appsignal.Tracer.current_span())
+
+      Logger.info("CURRENT_SPAN_23")
+      Logger.info( Appsignal.Tracer.current_span())
+
+      Logger.info("CURRENT_SPAN_24")
+      Logger.info( Appsignal.Tracer.current_span())
+
+      Logger.info("CURRENT_SPAN_25")
+      Logger.info( Appsignal.Tracer.current_span())
+
+      Logger.info("CURRENT_SPAN_26")
+      Logger.info( Appsignal.Tracer.current_span())
+    end)
+
+    Appsignal.instrument("slow_3", fn ->
+      :timer.sleep(1000)
+      Logger.info("CURRENT_SPAN_32")
+      Logger.info( Appsignal.Tracer.current_span())
+
+      Logger.info("CURRENT_SPAN_33")
+      Logger.info( Appsignal.Tracer.current_span())
+
+      Logger.info("CURRENT_SPAN_34")
+      Logger.info( Appsignal.Tracer.current_span())
+
+      Logger.info("CURRENT_SPAN_35")
+      Logger.info( Appsignal.Tracer.current_span())
+
+      Logger.info("CURRENT_SPAN_36")
+      Logger.info( Appsignal.Tracer.current_span())
+    end)
   end
 end
