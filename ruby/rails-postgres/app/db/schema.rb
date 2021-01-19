7be411fa6e6f9ce89c549109edb374965fcb3261
Tom de Bruijn <tom@tomdebruijn.com>
2021-01-19 11:15:13 +0100

    Update rails-postgres example to use PostgreSQL
    The rails postgres app didn't actually use the postgres database that
    was spun up alongside it. It was using sqlite.
    
    The app now uses the PostgreSQL database and runs the migrations
    automatically on boot so the user doesn't have to run the manually after
    start.

--- ruby/rails-postgres/app/db/schema.rb
+++ ruby/rails-postgres/app/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_11_08_223710) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.text "description"
