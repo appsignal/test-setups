728e479947762ebb8780a60151123406bdb2cc32
Tom de Bruijn <tom@tomdebruijn.com>
2021-01-22 11:24:51 +0100

    Add create-react-app example app (#14)
    Basic app that sends a demo sample. It's not very complete but is a
    skeleton for more scenarios that we can test, like sending private
    webpack sourcemaps.
    
    Add the JavaScript integration as one of the integrations managed by the
    repo.
    
    The `docker-compose.yml` file follows the new #15 standard.

---
+++ javascript/react/app/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import Appsignal from "./Appsignal";
+
+Appsignal.demo();
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to the React test app!
+        </p>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
