815890e835948bdbecc92ffadb821831cbd96c5a
Tom de Bruijn <tom@tomdebruijn.com>
2021-02-12 16:35:52 +0100

    Add method instrumentation example

--- ruby/rails-postgres/app/app/controllers/items_controller.rb
+++ ruby/rails-postgres/app/app/controllers/items_controller.rb
@@ -41,6 +41,7 @@ class ItemsController < ApplicationController
   # PATCH/PUT /items/1
   # PATCH/PUT /items/1.json
   def update
+    ItemUpdater.update_all([@item], :attributes => item_params)
     respond_to do |format|
       if @item.update(item_params)
         format.html { redirect_to @item, notice: 'Item was successfully updated.' }
