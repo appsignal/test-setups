974a28200d7f9593b066f7bb552ffe1cd850a5d5
Tom de Bruijn <tom@tomdebruijn.com>
2021-02-12 11:06:54 +0100

    Add basic webmachine test app

---
+++ ruby/webmachine/app/app.rb
@@ -0,0 +1,55 @@
+require "webmachine"
+require "appsignal"
+
+Appsignal.config = Appsignal::Config.new(
+  Dir.pwd,
+  "development"
+)
+Appsignal.start_logger
+Appsignal.start
+
+Webmachine.application.configure do |config|
+  config.port = ENV["PORT"]
+end
+
+class MyPerformanceResource < Webmachine::Resource
+  def to_html
+    sleep 1
+
+    <<~HTML
+      <html>
+        <body>
+          ZzZzZz
+        </body>
+      </html>
+    HTML
+  end
+end
+
+class MyErrorResource < Webmachine::Resource
+  def to_html
+    raise "Fatal Webmachine app error!"
+  end
+end
+
+class MyRootResource < Webmachine::Resource
+  def to_html
+    <<~HTML
+      <html>
+        <body>
+          <h1>Hello webmachine app!</h1>
+          <ul>
+            <li><a href="/slow">Slow request</a></li>
+            <li><a href="/error">Error request</a></li>
+          </ul>
+        </body>
+      </html>
+    HTML
+  end
+end
+
+Webmachine.application.routes do
+  add ["slow"], MyPerformanceResource
+  add ["error"], MyErrorResource
+  add [], MyRootResource
+end
