259c3b560c56e27aa956c06a98e0b2e3b4f67551
Tom de Bruijn <tom@tomdebruijn.com>
2021-04-21 12:12:13 +0200

    Update single-task apps
    The jruby and MRI apps are the same, but now both send metrics and
    erorrs. This makes the test app a bit more useful.

--- ruby/jruby-single-task/app/app.rb
+++ ruby/jruby-single-task/app/app.rb
@@ -3,6 +3,7 @@
 $stdout.sync = true
 $stderr.sync = true
 
+require "securerandom"
 # Setup AppSignal
 require "appsignal"
 require "appsignal/integrations/object"
@@ -19,20 +20,48 @@ at_exit do
   sleep 5
 end
 
+class CustomError < StandardError; end
+
 # The app itself
 class Monitor
-  def perform(param1, options = {}, foo:, bar:)
+  def perform(param1, options = {}, foo:, error:)
     Appsignal.instrument("perform.do_stuff") do
-      puts "Monitor is doing stuff. Params: #{param1}, #{options}, #{foo}, #{bar}"
+      puts "Monitor is doing stuff. Params: #{param1}, #{options}, #{foo}, #{error}"
       sleep 1
     end
+    Appsignal.increment_counter "test_counter", rand(1..3), "host" => "my-host"
+    Appsignal.set_gauge "test_gauge", rand(1..100), "host" => "my-host"
+    Appsignal.add_distribution_value "test_distribution", rand(1..100), "host" => "my-host"
+
+    raise CustomError, "Demo error" if error
   end
   appsignal_instrument_method :perform
 end
 
 20.times do |i|
-  Appsignal.monitor_transaction("perform_job.monitor", :class => "Monitor", :method => "loop") do
-    Monitor.new.perform("foo", { :foo => i }, foo: i, bar: i.even?)
+  # Test with monitor_transaction helper
+  begin
+    Appsignal.monitor_transaction("perform_job.monitor", :class => "MonitorTransaction", :method => "loop") do
+      Monitor.new.perform("foo", { :foo => i }, foo: i, error: i.even?)
+    end
+  rescue CustomError
+    # Do nothing, tracked by monitor_transaction
   end
+
+  # Test using transaction class directly, like most AppSignal integrations do
+  begin
+    transaction = Appsignal::Transaction.create(
+      SecureRandom.uuid,
+      Appsignal::Transaction::HTTP_REQUEST,
+      Appsignal::Transaction::GenericRequest.new({})
+    )
+    Monitor.new.perform("foo", { :foo => i }, foo: i, error: i.even?)
+  rescue => error
+    transaction.set_error(error)
+  ensure
+    transaction.set_action_if_nil("MonitorCustomTransaction#loop")
+    Appsignal::Transaction.complete_current!
+  end
+
   sleep 1
 end
