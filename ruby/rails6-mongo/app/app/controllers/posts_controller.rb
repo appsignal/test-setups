9f1182eaa49e4fe49e58d4adb7aac8f422b22a2d
Luismi Ramírez <luismi.ramirez@protonmail.com>
2022-02-01 17:06:42 +0100

    Add Rails v6 with Mongo setup
    This commits adds a new test setup consisting on Rails v6 + Mongoid.
    Didn't use Rails v7 as it isn't supported by Mongoid yet.
    
    The setup includes the test endpoints `slow` and `errors` and a couple
    of endpoints that allow you to list, show and create blog posts for
    testing purposes.

---
+++ ruby/rails6-mongo/app/app/controllers/posts_controller.rb
@@ -0,0 +1,25 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    Post.create(post_params)
+
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+end
