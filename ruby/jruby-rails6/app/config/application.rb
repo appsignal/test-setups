c71b0c954ca4352ea3353ba9706056e6a1702679
Luismi Ramírez <luismi.ramirez@protonmail.com>
2022-02-24 15:16:51 +0100

    Fix jruby-Rails6 CI
    The jruby-rails6 was running the Padrino tests by mistake. This commits
    updates it to run its own tests.

--- ruby/jruby-rails6/app/config/application.rb
+++ ruby/jruby-rails6/app/config/application.rb
@@ -11,6 +11,8 @@ module App
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.hosts << "app"
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
