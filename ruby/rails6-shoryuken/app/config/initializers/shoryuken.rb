12e68d49506cf7cafafb44a3d14d85a8d1cc7f4f
Luismi Ramírez <luismi.ramirez@protonmail.com>
2022-03-01 13:59:10 +0100

    Add Rails6 + Shoryuken test application
    This new app uses Rails6 + Shoryuken as the AJ adapter.
    It follows the current test setup schema.

---
+++ ruby/rails6-shoryuken/app/config/initializers/shoryuken.rb
@@ -0,0 +1,19 @@
+Shoryuken.configure_client do |config|
+  config.sqs_client = Aws::SQS::Client.new(
+    region: ENV["AWS_REGION"],
+    access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+    secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+    endpoint: ENV["SHORYUKEN_SQS_ENDPOINT"],
+    verify_checksums: false # Disable for mock server
+  )
+end
+
+Shoryuken.configure_server do |config|
+  config.sqs_client = Aws::SQS::Client.new(
+    region: ENV["AWS_REGION"],
+    access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+    secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+    endpoint: ENV["SHORYUKEN_SQS_ENDPOINT"],
+    verify_checksums: false # Disable for mock server
+  )
+end
