a2c0e444c4aa9c4bf0716318978bff3d9c2ea8cc
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2022-05-17 12:19:47 +0200

    Add appsignal_plug to dependencies

--- elixir/plug/app/mix.exs
+++ elixir/plug/app/mix.exs
@@ -19,10 +19,16 @@ defmodule App.MixProject do
     ]
   end
 
+  defp integration_path do
+    System.get_env("INTEGRATION_PATH", "../../integration")
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:appsignal, path: "#{integration_path()}/appsignal-elixir", override: true},
+      {:appsignal_plug, path: "#{integration_path()}/appsignal-elixir-plug", override: true}
     ]
   end
 end
