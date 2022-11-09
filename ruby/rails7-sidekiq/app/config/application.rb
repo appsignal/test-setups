28e6f1697f09faaf10bdf3e730f89482ff86df85
Tom de Bruijn <tom@tomdebruijn.com>
2022-11-09 14:55:46 +0100

    Update ruby/rails7-sidekiq/app/config/application.rb
    Co-authored-by: Noemi <45180344+unflxw@users.noreply.github.com>

--- ruby/rails7-sidekiq/app/config/application.rb
+++ ruby/rails7-sidekiq/app/config/application.rb
@@ -15,7 +15,8 @@ module TestApp
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
-    #
+    
+    config.hosts << "app"
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
   end
