cacda0b09bcf63d844680611284702eee95390a0
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2023-01-03 16:08:18 +0100

    Add handle_event for ThermostatLive

--- elixir/phoenix-example/app/lib/appsignal_phoenix_example_web/live/thermostat_live.ex
+++ elixir/phoenix-example/app/lib/appsignal_phoenix_example_web/live/thermostat_live.ex
@@ -5,10 +5,21 @@ defmodule AppsignalPhoenixExampleWeb.ThermostatLive do
   def render(assigns) do
     ~H"""
     Current temperature: <%= @temperature %>
+    <br>
+    <button phx-click="dec_temperature">-</button>
+    <button phx-click="inc_temperature">+</button>
     """
   end
 
   def mount(_params, _session, socket) do
     {:ok, assign(socket, :temperature, 20.0)}
+  end
+
+  def handle_event("dec_temperature", _value, socket) do
+    {:noreply, assign(socket, :temperature, socket.assigns.temperature - 1.0)}
+  end
+
+  def handle_event("inc_temperature", _value, socket) do
+    {:noreply, assign(socket, :temperature, socket.assigns.temperature + 1.0)}
   end
 end
