e6db840b16acd540854c47bd405c442361034924
Shairyar Baig <shairyarbaig@gmail.com>
2023-01-10 16:26:30 +0500

    Send logs to AppSignal

--- ruby/rails7-sidekiq/app/config/environment.rb
+++ ruby/rails7-sidekiq/app/config/environment.rb
@@ -2,7 +2,7 @@
 require_relative "application"
 
 # Use AppSignal's logger
-# Rails.logger = Appsignal::Logger.new("rails")
+Rails.logger = Appsignal::Logger.new("rails")
 
 # Initialize the Rails application.
 Rails.application.initialize!
