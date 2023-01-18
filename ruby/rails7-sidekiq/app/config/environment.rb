98211f5346f924d47cc9409887ee48d6c92374f7
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2023-01-18 15:05:47 +0100

    Hook up Appsignal::Logger

--- ruby/rails7-sidekiq/app/config/environment.rb
+++ ruby/rails7-sidekiq/app/config/environment.rb
@@ -1,5 +1,7 @@
 # Load the Rails application.
 require_relative "application"
 
+Rails.logger = Appsignal::Logger.new("rails", Logger::DEBUG)
+
 # Initialize the Rails application.
 Rails.application.initialize!
