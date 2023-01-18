412c6f503a818c1c65b1a8b454b696392c98ffd8
Tom de Bruijn <tom@tomdebruijn.com>
2023-01-18 10:57:47 +0100

    Fix processmon build on ARM macOS machines
    Make sure the builds are done in an emulated amd64 environment.
    Upgrade to Rust 1.66.1 to help with the successful install.

--- support/processmon/build.sh
+++ support/processmon/build.sh
@@ -3,7 +3,7 @@
 set -eu
 
 rm -f processmon
-docker build -t processmon .
+docker build --platform linux/amd64 -t processmon .
 docker run -i -v ${PWD}:/mount processmon:latest sh -s <<EOF
   cp /usr/local/cargo/bin/processmon /mount/
 EOF
