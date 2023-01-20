e257095f9434fc25cb901b6b8cec00d8ba1e9009
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2023-01-20 11:07:25 +0100

    Set log_formatter in development.rb

--- ruby/rails7-sidekiq/app/config/environments/development.rb
+++ ruby/rails7-sidekiq/app/config/environments/development.rb
@@ -67,4 +67,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Use default logging formatter so that PID and timestamp are not suppressed.
+  config.log_formatter = ::Logger::Formatter.new
 end
