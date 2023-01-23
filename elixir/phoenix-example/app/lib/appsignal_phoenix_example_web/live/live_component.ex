6e6143ef73622757e88713d97d694e2815ed6d82
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2023-01-23 15:36:21 +0100

    Add Ecto.Multi/constraint/live component test case

--- elixir/phoenix-example/app/lib/appsignal_phoenix_example_web/live/live_component.ex
+++ elixir/phoenix-example/app/lib/appsignal_phoenix_example_web/live/live_component.ex
@@ -3,12 +3,23 @@ defmodule AppsignalPhoenixExampleWeb.LiveComponent do
 
   def render(assigns) do
     ~H"""
+    <div>
     <button phx-click="raise_exception" phx-target={@myself}>raise component error</button>
+    <button phx-click="create_user" phx-target={@myself}>raise component constraint error</button>
+    </div>
     """
   end
 
   def handle_event("raise_exception", _value, socket) do
     raise "Component exception!"
+
+    {:noreply, socket}
+  end
+
+  def handle_event("create_user", _value, socket) do
+    Ecto.Multi.new()
+    |> Ecto.Multi.insert(:user, %AppsignalPhoenixExample.Accounts.User{name: "user", age: 10})
+    |> AppsignalPhoenixExample.Repo.transaction()
 
     {:noreply, socket}
   end
