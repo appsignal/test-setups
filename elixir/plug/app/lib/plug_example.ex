6d5f260a6bdf7e0c210457b83241e1a0f2d0daff
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2023-02-14 13:36:22 +0100

    Add /slow and /error endpoints to Plug app

--- elixir/plug/app/lib/plug_example.ex
+++ elixir/plug/app/lib/plug_example.ex
@@ -8,4 +8,14 @@ defmodule PlugExample do
   get "/" do
     send_resp(conn, 200, "Hello world")
   end
+
+  get "/slow" do
+    :timer.sleep(3000)
+
+    send_resp(conn, 200, "Welp, that took forever!")
+  end
+
+  get "/error" do
+    raise "Whoops!"
+  end
 end
