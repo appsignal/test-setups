e08c561722adbe1fa462ddeab3bc9c12dbca618a
Shairyar Baig <shairyarbaig@gmail.com>
2023-02-23 16:47:19 +0500

    Send oban logs to AppSignal

--- elixir/plug-oban/app/lib/add_task.ex
+++ elixir/plug-oban/app/lib/add_task.ex
@@ -5,19 +5,24 @@ defmodule PlugOban.AddTask do
 
   @impl Oban.Worker
   def perform(%Oban.Job{args: %{"numbers" => numbers} = args}) do
+    Appsignal.Logger.debug("app", "Received add job with numbers: #{inspect(numbers)}")
+
     Logger.debug("Received add job with numbers: #{inspect(numbers)}")
 
     sum = Enum.sum(numbers)
+    Appsignal.Logger.debug("app", "Added up numbers to: #{inspect(sum)}")
     Logger.debug("Added up numbers to: #{inspect(sum)}")
 
     Process.sleep(sum)
+    Appsignal.Logger.debug("app", "Slept for #{inspect(sum)} milliseconds")
     Logger.debug("Slept for #{inspect(sum)} milliseconds")
 
     if Map.get(args, "fail") do
+      Appsignal.Logger.debug("app", "Task failed!")
       Logger.debug("Task failed!")
       raise PlugOban.TaskError
     end
-
+    Appsignal.Logger.debug("app", "Task completed!")
     Logger.debug("Task completed!")
     :ok
   end
