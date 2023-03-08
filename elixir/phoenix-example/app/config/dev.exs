74ef086768eef944d90b24db17e52fa4a1645a53
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2023-03-08 13:29:28 +0100

    Set up logft in Phoenix app to test support

--- elixir/phoenix-example/app/config/dev.exs
+++ elixir/phoenix-example/app/config/dev.exs
@@ -66,7 +66,8 @@ config :appsignal_phoenix_example, AppsignalPhoenixExampleWeb.Endpoint,
   ]
 
 # Do not include metadata nor timestamps in development logs
-config :logger, :console, format: "[$level] $message\n"
+config :logger, :console,
+  format: {LogfmtEx, :format}
 
 # Set a higher stacktrace during development. Avoid configuring such
 # in production as building large stacktraces may be expensive.
