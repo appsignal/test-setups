12e3ee7266841e4d533ba095aea57d479aba44a9
Noemi <45180344+unflxw@users.noreply.github.com>
2023-03-17 11:38:43 +0100

    Use `TaggedLogging` to support Rails log tags
    By wrapping the AppSignal logger in `ActiveSupport::TaggedLogging`,
    we support Rails' `config.log_tags` option out of the box. Other
    bits of Rails, such as ActionMailer, also throw tags at the logger.

--- ruby/rails7-sidekiq/app/config/application.rb
+++ ruby/rails7-sidekiq/app/config/application.rb
@@ -11,6 +11,7 @@ module TestApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.log_tags = [:request_id]
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
--- ruby/rails7-sidekiq/app/config/environment.rb
+++ ruby/rails7-sidekiq/app/config/environment.rb
@@ -1,5 +1,7 @@
 # Load the Rails application.
 require_relative "application"
 
+Rails.logger = ActiveSupport::TaggedLogging.new(Appsignal::Logger.new("rails"))
+
 # Initialize the Rails application.
 Rails.application.initialize!
