fc310bed25764d3ac995e7720d7794fcdff3e0b8
Tom de Bruijn <tom@tomdebruijn.com>
2023-04-20 11:56:14 +0200

    Add Rails error reporter example to Sidekiq app
    Add an example how the Rails error reporter works in a Sidekiq app so we
    can test it more easily in a real world app.

--- ruby/rails7-sidekiq/app/app/workers/error_worker.rb
+++ ruby/rails7-sidekiq/app/app/workers/error_worker.rb
@@ -3,6 +3,9 @@ class ErrorWorker
   sidekiq_options :retry => 3
 
   def perform(argument = nil, options = {})
+    Rails.error.handle do
+      1 + '1' # raises TypeError
+    end
     raise "Error: #{argument}"
   end
 end
