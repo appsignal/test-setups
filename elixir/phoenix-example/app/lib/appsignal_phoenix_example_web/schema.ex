73c8ca8777662bdce9cfbfa8f52b15ca994f281b
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2023-06-23 10:01:08 +0200

    Add Absinthe endpoint (#121)

---
+++ elixir/phoenix-example/app/lib/appsignal_phoenix_example_web/schema.ex
@@ -0,0 +1,23 @@
+defmodule AppsignalPhoenixExampleWeb.Schema do
+  use Absinthe.Schema
+
+  @desc "An item"
+  object :item do
+    field :id, :id
+    field :name, :string
+  end
+
+  @items %{
+    "foo" => %{id: "foo", name: "Foo"},
+    "bar" => %{id: "bar", name: "Bar"}
+  }
+
+  query do
+    field :item, :item do
+      arg :id, non_null(:id)
+      resolve fn %{id: item_id}, _ ->
+	{:ok, @items[item_id]}
+      end
+    end
+  end
+end
