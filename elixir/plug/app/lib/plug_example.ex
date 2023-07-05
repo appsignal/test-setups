eb42cefdf5918ab37e60494c6ad5d87020625990
Noemi <45180344+unflxw@users.noreply.github.com>
2023-07-05 09:23:36 +0200

    Add custom instrumentation Plug endpoint
    Add an endpoint that uses custom instrumentation to set params --
    these params should not be overriden with appsignal-elixir-plug#37.

--- elixir/plug/app/lib/plug_example.ex
+++ elixir/plug/app/lib/plug_example.ex
@@ -18,4 +18,11 @@ defmodule PlugExample do
   get "/error" do
     raise "Whoops!"
   end
+
+  get "/custom" do
+    Appsignal.Tracer.root_span()
+    |> Appsignal.Span.set_sample_data("params", %{"wow_such_custom" => "amaze"})
+
+    send_resp(conn, 200, "Reported custom sample data")
+  end
 end
