e05c355541597c7774b0e3113e20d4cc8c2abd40
Tom de Bruijn <tom@tomdebruijn.com>
2023-08-17 11:42:28 +0200

    Log to both STDOUT and AppSignal in Sidekiq app (#133)
    Make development/testing easier by logging to both the terminal/STDOUT
    and AppSignal (what we also want to test if it works). Without this
    change it would only log to AppSignal, which is delayed and not part of
    a local dev workflow.
    
    Use ActiveSupport::Logger.broadcast to wrap loggers and send logs to
    multiple backends.
    
    Move the Rails logger config from `config/environment.rb` to
    an initializer as documented in
    https://github.com/appsignal/appsignal-docs/pull/1453.

--- ruby/rails7-sidekiq/app/config/initializers/sidekiq.rb
+++ ruby/rails7-sidekiq/app/config/initializers/sidekiq.rb
@@ -1,4 +1,5 @@
 Sidekiq.configure_server do |config|
-  config.logger = Appsignal::Logger.new("sidekiq")
+  console_logger = ActiveSupport::Logger.new(STDOUT)
+  config.logger = console_logger.extend(ActiveSupport::Logger.broadcast(Appsignal::Logger.new("sidekiq")))
   config.logger.formatter = Sidekiq::Logger::Formatters::WithoutTimestamp.new
 end
