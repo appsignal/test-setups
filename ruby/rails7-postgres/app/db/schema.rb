9bfd1827a99e1da110f5c30d38e88b615a1cc2c0
Tom de Bruijn <tom@tomdebruijn.com>
2023-08-18 09:48:57 +0200

    Regenerate Rails 7 Sidekiq app (#137)
    Make sure we're using a fresh Rails app (with all the customization
    copied to the new app) so it's built like a new Rails 7 app without any
    upgrade quirks.

--- ruby/rails7-postgres/app/db/schema.rb
+++ ruby/rails7-postgres/app/db/schema.rb
@@ -10,16 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_08_223710) do
-
+ActiveRecord::Schema[7.0].define(version: 2020_11_08_223710) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.text "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
