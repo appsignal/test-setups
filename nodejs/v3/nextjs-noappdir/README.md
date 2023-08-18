47e2a495e9420666ca86f70f90ddfd90c3fd6f71
Tom de Bruijn <tom@tomdebruijn.com>
2023-08-18 15:24:19 +0200

    Add front-end monitoring to Next.js page router (#136)
    * Add front-end monitoring to Next.js page router
    
    For demonstration (and completeness) purposes, add the front-end React
    ErrorBoundary to the Next.js app using the page router (also known as
    `noappdir`).
    
    * Fix CI build by setting the frontend key in CI

---
+++ nodejs/v3/nextjs-noappdir/README.md
@@ -0,0 +1,5 @@
+# Next.js Page router app
+
+For front-end error reporting to work, add a `appsignal_key.env` file (example
+can be found in `appsignal_key.env.example`), and add the front-end API key for
+the specific existing app you want to add the front-end errors to.
--- nodejs/v3/nextjs-noappdir/app/src/pages/_app.tsx
+++ nodejs/v3/nextjs-noappdir/app/src/pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css"
+import type { AppProps } from "next/app"
+import { appsignal } from "./_appsignal";
+import { ErrorBoundary } from "@appsignal/react";
+
+const FallbackComponent = () => (
+  <div>Uh oh! There was a client-side error :(</div>
+);
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+  <ErrorBoundary
+    instance={appsignal}
+    tags={{ tag: "value" }}
+    fallback={(_error: Error) => <FallbackComponent />}
+  >
+      <Component {...pageProps} />
+    </ErrorBoundary>
+  )
 }
