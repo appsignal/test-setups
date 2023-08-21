a622c3f3a663b1abde168df0a1572e15260af710
Luismi Ramírez <luismi.ramirez@protonmail.com>
2023-08-21 16:05:34 +0200

    Remove unnecessary elements from Remix app
    - NPM installation
    - Ignore build paths in processmon
    - Remove defaults from config file

--- nodejs/v3/remix/app/appsignal.cjs
+++ nodejs/v3/remix/app/appsignal.cjs
@@ -4,9 +4,6 @@ const { RemixInstrumentation } = require("opentelemetry-instrumentation-remix");
 new Appsignal({
   active: true,
   name: "opentelemetry-remix",
-  logLevel: "trace",
-  log: "file",
-  logPath: "/tmp",
   additionalInstrumentations: [new RemixInstrumentation()],
   disableDefaultInstrumentations: ["@opentelemetry/instrumentation-express"]
 });
