041eedec3a0032e171e295cc9b38a26a5510345b
Tom de Bruijn <tom@tomdebruijn.com>
2023-08-25 13:50:12 +0200

    Add more metrics to Python Flask test app
    Send values for all the different types of metrics we support.

--- python/flask/app/app.py
+++ python/flask/app/app.py
@@ -6,12 +6,38 @@ appsignal.start()
 from flask import Flask
 app = Flask(__name__)
 
+from typing import Iterable
 from opentelemetry import metrics
+from opentelemetry.metrics import Observation, CallbackOptions
+
+from random import randrange
 
 meter = metrics.get_meter("flask-app-custom-metrics")
 
-request_counter = meter.create_counter(
-    "request.count", unit="1", description="Counts the amount of requests received"
+monotonic_counter_with_tags = meter.create_counter(
+    "monotonic_counter_with_tags",
+    unit="1",
+    description="monotonic counter with tags"
+)
+monotonic_counter = meter.create_counter(
+    "monotonic_counter", unit="1", description="monotonic counter"
+)
+non_monotonic_counter = meter.create_up_down_counter(
+    "non_monotonic_counter",
+    unit="1",
+    description="non monotonic counter"
+)
+
+
+def gauge_callback(options: CallbackOptions) -> Iterable[Observation]:
+    return [Observation(100 + randrange(50))]
+
+
+meter.create_observable_gauge(
+    "some_gauge",
+    callbacks=[gauge_callback],
+    unit="1",
+    description="test gauge"
 )
 
 @app.route("/")
@@ -43,5 +69,8 @@ def hello(name):
 
 @app.route("/metrics")
 def metrics():
-  request_counter.add(1)
-  return "<p>Emitted some custom metrics!</p>"
+    monotonic_counter_with_tags.add(1, {"tag1": "value1", "tag2": "value2"})
+    monotonic_counter.add(1)
+    non_monotonic_counter.add(10)
+    non_monotonic_counter.add(-5)
+    return "<p>Emitted some custom metrics!</p>"
