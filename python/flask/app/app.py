988e9aeab35313277e5d26579c7dccc432039db6
Tom de Bruijn <tom@tomdebruijn.com>
2023-09-01 08:56:40 +0200

    Add flask custom metrics endpoint (#142)
    * Add endpoint to test custom metrics
    
    * Add more metrics to Python Flask test app
    
    Send values for all the different types of metrics we support.
    
    ---------
    
    Co-authored-by: Noemi <45180344+unflxw@users.noreply.github.com>

--- python/flask/app/app.py
+++ python/flask/app/app.py
@@ -6,6 +6,40 @@ appsignal.start()
 from flask import Flask
 app = Flask(__name__)
 
+from typing import Iterable
+from opentelemetry import metrics
+from opentelemetry.metrics import Observation, CallbackOptions
+
+from random import randrange
+
+meter = metrics.get_meter("flask-app-custom-metrics")
+
+monotonic_counter_with_tags = meter.create_counter(
+    "monotonic_counter_with_tags",
+    unit="1",
+    description="monotonic counter with tags"
+)
+monotonic_counter = meter.create_counter(
+    "monotonic_counter", unit="1", description="monotonic counter"
+)
+non_monotonic_counter = meter.create_up_down_counter(
+    "non_monotonic_counter",
+    unit="1",
+    description="non monotonic counter"
+)
+
+
+def gauge_callback(options: CallbackOptions) -> Iterable[Observation]:
+    return [Observation(100 + randrange(50))]
+
+
+meter.create_observable_gauge(
+    "some_gauge",
+    callbacks=[gauge_callback],
+    unit="1",
+    description="test gauge"
+)
+
 @app.route("/")
 def home():
     return """
@@ -15,6 +49,7 @@ def home():
         <li><a href="/slow">/slow: Trigger a slow request</a></li>
         <li><a href="/error">/error: Trigger an error</a></li>
         <li><a href="/hello/world">/hello/&lt;name&gt;: Use parameterised routing</a></li>
+        <li><a href="/metrics">/metrics: Emit custom metrics</a></li>
       </ul>
     """
 
@@ -31,3 +66,11 @@ def error():
 @app.route("/hello/<name>")
 def hello(name):
     return f"<p>Hello, {name}!"
+
+@app.route("/metrics")
+def metrics():
+    monotonic_counter_with_tags.add(1, {"tag1": "value1", "tag2": "value2"})
+    monotonic_counter.add(1)
+    non_monotonic_counter.add(10)
+    non_monotonic_counter.add(-5)
+    return "<p>Emitted some custom metrics!</p>"
