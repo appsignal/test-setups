7a97857303394a63d1e2942a09876cc96462730f
Tom de Bruijn <tom@tomdebruijn.com>
2023-09-01 11:17:19 +0200

    Remove Rails 6 Sidekiq app (#146)
    It suddenly stopped installing, because of a node-sass package install
    issue. It was installing node-sass 4.x, which is 7 years old. I'm not
    going to put a lot of effort in making that work when we have a Rails 7
    Sidekiq app as well.

--- ruby/rails6-sidekiq/app/app/mailers/performance_mailer.rb
+++
@@ -1,8 +0,0 @@
-class PerformanceMailer < ApplicationMailer
-  def test(*_args)
-    mail(
-      :to => "test@example.com",
-      :subject => "Welcome to My Awesome Site"
-    )
-  end
-end
--- ruby/rails6-sidekiq/app/app/workers/active_job_performance_worker.rb
+++
@@ -1,7 +0,0 @@
-class ActiveJobPerformanceWorker < ActiveJob::Base
-  queue_as :default
-
-  def perform(argument = nil, options = {})
-    puts "delivered #{argument}!"
-  end
-end
