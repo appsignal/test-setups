7a3646f0e393078563a48104c3bcf0192b34610a
Tom de Bruijn <tom@tomdebruijn.com>
2023-12-15 09:14:25 +0100

    Add Ruby Sinatra Alpine app (#175)
    Help debug an Alpine Linux issue.

---
+++ ruby/sinatra-alpine/app/app.rb
@@ -0,0 +1,33 @@
+require "active_support"
+require "action_mailer"
+require "sinatra"
+require "appsignal"
+require "appsignal/integrations/sinatra"
+
+get "/" do
+  time = Time.now.strftime("%H:%M")
+  <<~HTML
+    <h1>Sinatra example app</h1>
+
+    <ul>
+      <li><a href="/slow?time=#{time}">Slow request</a></li>
+      <li><a href="/error?time=#{time}">Error request</a></li>
+      <li><a href="/item/123?time=#{time}">Route with param request</a></li>
+    </ul>
+  HTML
+end
+
+get "/slow" do
+  sleep 3
+  "ZzZzZzZ.."
+end
+
+get "/error" do
+  Appsignal.add_breadcrumb("test", "action")
+  Appsignal.add_breadcrumb("category", "action", "message", { "metadata_key" => "some value" })
+  raise "error"
+end
+
+get "/item/:id" do
+  "Item with id #{params[:id]}"
+end
