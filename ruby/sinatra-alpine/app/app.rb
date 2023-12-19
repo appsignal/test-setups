1a13b3927dfaca159e4fe1a9df795b5193e7635a
Noemi <45180344+unflxw@users.noreply.github.com>
2023-12-19 15:35:08 +0100

    Add Sinatra + Alpine test setup

---
+++ ruby/sinatra-alpine/app/app.rb
@@ -0,0 +1,39 @@
+require "active_support"
+require "action_mailer"
+require "sinatra"
+require "appsignal"
+require "appsignal/integrations/sinatra"
+
+get "/" do
+  time = Time.now.strftime("%H:%M")
+  <<~HTML
+    <h1>Sinatra example app</h1>
+
+    <ul>
+      <li><a href="/slow?time=#{time}">Slow request</a></li>
+      <li><a href="/error?time=#{time}">Error request</a></li>
+      <li><a href="/item/123?time=#{time}">Route with param request</a></li>
+    </ul>
+  HTML
+end
+
+get "/slow" do
+  sleep 3
+  "ZzZzZzZ.."
+end
+
+def original_error
+  raise "I am the original error!"
+end
+
+get "/error" do
+  Appsignal.add_breadcrumb("test", "action")
+  Appsignal.add_breadcrumb("category", "action", "message", { "metadata_key" => "some value" })
+  original_error
+rescue
+  raise "I am a wrapper error!"
+end
+
+get "/item/:id" do
+  "Item with id #{params[:id]}"
+end
