75287a3d94741328896a11bf40311b7a5fb09093
Noemi <45180344+unflxw@users.noreply.github.com>
2023-12-20 17:00:01 +0100

    Use Semantic Logger with AppSignal

--- ruby/rails7-sidekiq/app/config/initializers/logging.rb
+++ ruby/rails7-sidekiq/app/config/initializers/logging.rb
@@ -1,4 +1,2 @@
-console_logger = ActiveSupport::Logger.new(STDOUT)
-appsignal_logger = ActiveSupport::TaggedLogging.new(Appsignal::Logger.new("rails"))
-Rails.application.config.log_tags = [:request_id]
-Rails.logger = ActiveSupport::BroadcastLogger.new(console_logger, appsignal_logger)
+appsignal_logger = ActiveSupport::TaggedLogging.new(Appsignal::Logger.new("rails", format: Appsignal::Logger::LOGFMT))
+SemanticLogger.add_appender(logger: appsignal_logger, formatter: :logfmt)
--- ruby/rails7-sidekiq/app/config/initializers/sidekiq.rb
+++ ruby/rails7-sidekiq/app/config/initializers/sidekiq.rb
@@ -1,6 +1,9 @@
-Sidekiq.configure_server do |config|
-  console_logger = ActiveSupport::Logger.new(STDOUT)
-  appsignal_logger = Appsignal::Logger.new("sidekiq")
-  config.logger = ActiveSupport::BroadcastLogger.new(console_logger, appsignal_logger)
-  config.logger.formatter = Sidekiq::Logger::Formatters::WithoutTimestamp.new
-end
+# Not necessary -- `rails_semantic_logger` automatically configures
+# itself for Sidekiq.
+#
+# Sidekiq.configure_server do |config|
+#   console_logger = ActiveSupport::Logger.new(STDOUT)
+#   appsignal_logger = Appsignal::Logger.new("sidekiq")
+#   config.logger = ActiveSupport::BroadcastLogger.new(console_logger, appsignal_logger)
+#   config.logger.formatter = Sidekiq::Logger::Formatters::WithoutTimestamp.new
+# end
