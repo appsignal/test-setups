6a5efcf6ac3314784854e3fbed039feb9e85eaba
Noemi <45180344+unflxw@users.noreply.github.com>
2024-01-10 14:15:01 +0100

    Create Plug + Ecto test setup
    This commit only copy-pastes the existing plain Plug and renames
    it to `plug-ecto` at the appropriate places. No Ecto-specific
    changes have been added.

---
+++ elixir/plug-ecto/app/lib/plug_example.ex
@@ -0,0 +1,79 @@
+defmodule PlugExample do
+  use Plug.Router
+  use Appsignal.Plug
+
+  plug :match
+  plug :dispatch
+
+  get "/" do
+    send_resp(conn, 200, "Hello world")
+  end
+
+  get "/slow" do
+    :timer.sleep(3000)
+
+    send_resp(conn, 200, "Welp, that took forever!")
+  end
+
+  get "/error" do
+    raise "Whoops!"
+  end
+
+  get "/custom_instrumentation" do
+    Appsignal.Span.set_namespace(Appsignal.Tracer.root_span(), "custom")
+
+    Appsignal.Span.set_sample_data(
+      Appsignal.Tracer.root_span(),
+      "tags",
+      %{
+        tag1: "value1",
+        tag2: 123
+      }
+    )
+
+    Appsignal.Span.set_sample_data(
+      Appsignal.Tracer.root_span(),
+      "params",
+      %{
+        query: "something to search for",
+        language: "en_US"
+      }
+    )
+
+    Appsignal.Span.set_sample_data(
+      Appsignal.Tracer.root_span(),
+      "session_data",
+      %{
+        session: %{
+          user_id: "123",
+          menu_collapsed: true
+        }
+      }
+    )
+
+    Appsignal.Span.set_sample_data(
+      Appsignal.Tracer.root_span(),
+      "custom_data",
+      %{
+        custom: %{
+          data: "test",
+          more: "data"
+        }
+      }
+    )
+
+    Appsignal.instrument("event.custom", fn ->
+      :timer.sleep(100)
+
+      Appsignal.instrument("nested.custom", fn ->
+        :timer.sleep(100)
+      end)
+
+      Appsignal.instrument("Build complicated SQL query", "prepare_query.sql", fn span ->
+        Appsignal.Span.set_sql(span, "SOME complicate SQL query")
+      end)
+    end)
+
+    send_resp(conn, 200, "Tracked some code with custom instrumentation")
+  end
+end
