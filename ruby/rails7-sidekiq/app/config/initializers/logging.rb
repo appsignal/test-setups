414a97f60c1dfeaa0139aafaf5bef7c1aac07d3f
Tom de Bruijn <tom@tomdebruijn.com>
2024-02-14 14:59:58 +0100

    Fix Rails logger config in Rails 7 Sidekiq app
    Use the latest documented method otherwise I can't queue ActiveJob
    jobs.

--- ruby/rails7-sidekiq/app/config/initializers/logging.rb
+++ ruby/rails7-sidekiq/app/config/initializers/logging.rb
@@ -1,4 +1,3 @@
-console_logger = ActiveSupport::Logger.new(STDOUT)
-appsignal_logger = ActiveSupport::TaggedLogging.new(Appsignal::Logger.new("rails"))
 Rails.application.config.log_tags = [:request_id]
-Rails.logger = ActiveSupport::BroadcastLogger.new(console_logger, appsignal_logger)
+appsignal_logger = Appsignal::Logger.new("rails")
+Rails.logger.broadcast_to(appsignal_logger)
