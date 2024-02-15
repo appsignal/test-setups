c595dcbee1d5cc862a0a75d8eeb5ad221e39bff4
Tom de Bruijn <tom@tomdebruijn.com>
2024-02-15 09:36:53 +0100

    Combine examples list for goodjob app
    Show all tests on one page.

--- ruby/rails7-goodjob/app/app/controllers/jobs_controller.rb
+++ ruby/rails7-goodjob/app/app/controllers/jobs_controller.rb
@@ -1,18 +1,14 @@
 class JobsController < ApplicationController
-  def index
-    Rails.logger.warn "Loading jobs index"
-  end
-
   def slow
     PerformanceJob.perform_later("Slow argument", :options => "something")
     Rails.logger.warn "Queued PerformanceJob"
 
-    redirect_to jobs_path, :notice => "Queued PerformanceJob"
+    redirect_to root_path, :notice => "Queued PerformanceJob"
   end
 
   def error
     ErrorJob.perform_later("Error argument", :options => "something")
     Rails.logger.warn "Queued ErrorJob"
-    redirect_to jobs_path, :notice => "Queued ErrorJob"
+    redirect_to root_path, :notice => "Queued ErrorJob"
   end
 end
