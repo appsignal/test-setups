9d4b452aa56614d4aee4ad2368c04759f1d9e37b
Tom de Bruijn <tom@tomdebruijn.com>
2024-02-15 13:14:01 +0100

    Add Rails 6 streaming test routes
    Similar to the Rails 7 app, test these streaming responses for Rails 6.
    
    Remove rack-mini-profiler, because the streaming won't work with it.

--- ruby/rails6-mysql/app/app/controllers/tests_controller.rb
+++ ruby/rails6-mysql/app/app/controllers/tests_controller.rb
@@ -6,4 +6,39 @@ class TestsController < ApplicationController
   def slow
     sleep 3
   end
+
+  class StreamingSlowBody
+    def each
+      sleep 1
+      yield "1"
+      sleep 1
+      yield "2"
+      sleep 1
+      yield "3"
+      sleep 1
+      yield "4"
+    end
+  end
+
+  def streaming_slow
+    headers["Content-Length"] = "4"
+    headers["Last-Modified"] = Time.now.httpdate
+    self.response_body = StreamingSlowBody.new
+  end
+
+  class StreamingErrorBody
+    def each
+      yield "1"
+      sleep 0.5
+      yield "2"
+      yield "3"
+      raise "Rails error in streaming body"
+    end
+  end
+
+  def streaming_error_in_body
+    headers["Content-Length"] = "4"
+    headers["Last-Modified"] = Time.now.httpdate
+    self.response_body = StreamingErrorBody.new
+  end
 end
--- ruby/rails6-mysql/app/config/routes.rb
+++ ruby/rails6-mysql/app/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get "/slow", to: "tests#slow"
   get "/error", to: "tests#error"
+  get "/streaming/slow", to: "tests#streaming_slow"
+  get "/streaming/error", to: "tests#streaming_error_in_body"
 
   resources :posts, :except => %i[edit update destroy]
+
+  root to: "tests#index"
 end
