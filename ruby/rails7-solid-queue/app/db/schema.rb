0747d11a602b708d8bf21c85cefe0a6f4fb739fb
Jeff Kreeftmeijer <jeff@kreeft.me>
2024-03-29 16:42:47 +0100

    Drop delayed_jobs table from schema

--- ruby/rails7-solid-queue/app/db/schema.rb
+++ ruby/rails7-solid-queue/app/db/schema.rb
@@ -10,24 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_28_184202) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_29_095325) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "delayed_jobs", force: :cascade do |t|
-    t.integer "priority", default: 0, null: false
-    t.integer "attempts", default: 0, null: false
-    t.text "handler", null: false
-    t.text "last_error"
-    t.datetime "run_at"
-    t.datetime "locked_at"
-    t.datetime "failed_at"
-    t.string "locked_by"
-    t.string "queue"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.index ["priority", "run_at"], name: "delayed_jobs_priority"
-  end
 
   create_table "solid_queue_blocked_executions", force: :cascade do |t|
     t.bigint "job_id", null: false
