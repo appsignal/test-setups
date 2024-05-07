aaf044fef2f0091dcfae7a7a7f483a63b7fc6c07
Jeff Kreeftmeijer <jeff@kreeft.me>
2024-05-07 17:15:20 +0200

    Set up opentelemetry to push data to agent

--- elixir/phoenix-opentelemetry/app/lib/elixir_phoenix_opentelemetry/application.ex
+++ elixir/phoenix-opentelemetry/app/lib/elixir_phoenix_opentelemetry/application.ex
@@ -20,6 +20,9 @@ defmodule ElixirPhoenixOpentelemetry.Application do
       ElixirPhoenixOpentelemetryWeb.Endpoint
     ]
 
+    # OpentelemetryBandit.setup()
+    OpentelemetryPhoenix.setup()#adapter: :bandit)
+
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: ElixirPhoenixOpentelemetry.Supervisor]
