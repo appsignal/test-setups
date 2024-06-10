4dedc7838142ed6e731a0e4ee26b628520d0e35d
Noemi <45180344+unflxw@users.noreply.github.com>
2024-06-10 13:27:45 +0200

    Use AppSignal fork from the integration
    Instead of fetching my personal fork of the BullMQ instrumentation
    and adding it manually to the `additionalDependencies`, newer
    versions of the integration have the AppSignal fork of the
    instrumentation as a dependency, so this setup does not need to
    be done.

--- nodejs/express-bullmq/app/src/appsignal.cjs
+++ nodejs/express-bullmq/app/src/appsignal.cjs
@@ -1,9 +1,5 @@
 const { Appsignal } = require("@appsignal/nodejs");
-const { BullMQInstrumentation } = require("@jenniferplusplus/opentelemetry-instrumentation-bullmq");
 
 new Appsignal({
-  active: true,
-  additionalInstrumentations: [
-    new BullMQInstrumentation()
-  ]
+  active: true
 });
