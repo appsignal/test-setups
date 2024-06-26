7d4fbf7766706e03ed668a227dbcadcd5fa7a5b6
Tom de Bruijn <tom@tomdebruijn.com>
2024-06-26 08:57:55 +0200

    Add Hanami app to the CI build
    The Hanami app wasn't added to the CI build yet and it didn't include
    routes for an example error and slow endpoint.

--- ruby/hanami2-postgres/app/app/action.rb
+++ ruby/hanami2-postgres/app/app/action.rb
@@ -5,5 +5,17 @@ require "hanami/action"
 
 module Hanami2Postgres
   class Action < Hanami::Action
+    handle_exception StandardError => :handle_standard_error
+
+    private
+
+    def handle_standard_error(request, response, exception)
+      # Report the error to AppSignal
+      Appsignal.set_error(exception)
+
+      # Render custom error page
+      response.status = 500
+      response.body = "Sorry, something went wrong handling your request"
+    end
   end
 end
