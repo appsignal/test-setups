7ba969dcfea0d71a24117841beddad06ca22e46a
Tom de Bruijn <tom@tomdebruijn.com>
2024-10-24 16:35:27 +0200

    Add Elixir OpenTelemetry to Semaphore build

--- elixir/phoenix-opentelemetry/app/lib/elixir_phoenix_opentelemetry_web/controllers/page_controller.ex
+++ elixir/phoenix-opentelemetry/app/lib/elixir_phoenix_opentelemetry_web/controllers/page_controller.ex
@@ -1,9 +1,16 @@
 defmodule ElixirPhoenixOpentelemetryWeb.PageController do
   use ElixirPhoenixOpentelemetryWeb, :controller
 
-  def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
+  def index(conn, _params) do
+    render(conn, "index.html")
+  end
+
+  def slow(conn, _params) do
+    :timer.sleep(3000)
+    render(conn, "slow.html")
+  end
+
+  def error(_conn, _params) do
+    raise "This is a Phoenix error!"
   end
 end
--- elixir/phoenix-opentelemetry/app/lib/elixir_phoenix_opentelemetry_web/router.ex
+++ elixir/phoenix-opentelemetry/app/lib/elixir_phoenix_opentelemetry_web/router.ex
@@ -2,22 +2,24 @@ defmodule ElixirPhoenixOpentelemetryWeb.Router do
   use ElixirPhoenixOpentelemetryWeb, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_live_flash
-    plug :put_root_layout, html: {ElixirPhoenixOpentelemetryWeb.Layouts, :root}
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_live_flash)
+    plug(:put_root_layout, html: {ElixirPhoenixOpentelemetryWeb.Layouts, :root})
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/", ElixirPhoenixOpentelemetryWeb do
-    pipe_through :browser
+    pipe_through(:browser)
 
-    get "/", PageController, :home
+    get("/", PageController, :index)
+    get("/slow", PageController, :slow)
+    get("/error", PageController, :error)
   end
 
   # Other scopes may use custom stacks.
@@ -35,10 +37,10 @@ defmodule ElixirPhoenixOpentelemetryWeb.Router do
     import Phoenix.LiveDashboard.Router
 
     scope "/dev" do
-      pipe_through :browser
+      pipe_through(:browser)
 
-      live_dashboard "/dashboard", metrics: ElixirPhoenixOpentelemetryWeb.Telemetry
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
+      live_dashboard("/dashboard", metrics: ElixirPhoenixOpentelemetryWeb.Telemetry)
+      forward("/mailbox", Plug.Swoosh.MailboxPreview)
     end
   end
 end
