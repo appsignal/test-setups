a1ddef9cc8efea896ec60b89f41151ac0efc415a
Tom de Bruijn <tom@tomdebruijn.com>
2024-11-06 12:43:08 +0100

    Install AppSignal gem in Hanami app

--- ruby/hanami2-postgres/app/app/action.rb
+++ ruby/hanami2-postgres/app/app/action.rb
@@ -8,5 +8,18 @@ module ExampleApp
   class Action < Hanami::Action
     # Provide `Success` and `Failure` for pattern matching on operation results
     include Dry::Monads[:result]
+
+    handle_exception StandardError => :handle_standard_error
+
+    private
+
+    def handle_standard_error(request, response, exception)
+      # Report the error to AppSignal
+      Appsignal.report_error(exception)
+
+      # Render custom error page
+      response.status = 500
+      response.body = "Sorry, something went wrong handling your request"
+    end
   end
 end
--- ruby/hanami2-postgres/app/config.ru
+++ ruby/hanami2-postgres/app/config.ru
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+require "appsignal"
 require "hanami/boot"
+
+# Load the Hanami integration
+Appsignal.load(:hanami)
+# Start AppSignal
+Appsignal.start
 
 run Hanami.app
