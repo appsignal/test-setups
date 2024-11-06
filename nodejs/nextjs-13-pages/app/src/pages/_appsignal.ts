dddab28657fcc42c12ff4b9fdf99a225ad8d8d83
Tom de Bruijn <tom@tomdebruijn.com>
2024-11-06 13:20:39 +0100

    Remove Next.js 13 app using the pages router (#239)

--- nodejs/nextjs-13-pages/app/src/pages/_appsignal.ts
+++
@@ -1,9 +0,0 @@
-import Appsignal from "@appsignal/javascript";
-
-const key = process.env.NEXT_PUBLIC_APPSIGNAL_FRONTEND_API_KEY
-if (!key) {
-  console.error("No NEXT_PUBLIC_APPSIGNAL_FRONTEND_API_KEY found! Please add the key to the app's `nodejs/v3/nextjs-noappdir/appsignal_key.env` file")
-}
-export const appsignal = new Appsignal({
-  key
-});
