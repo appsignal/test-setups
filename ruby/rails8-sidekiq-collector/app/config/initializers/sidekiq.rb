00c7f73ca97c284ce52a6b11ee97bc11102c8bf3
Tom de Bruijn <tom@tomdebruijn.com>
2025-02-06 16:32:47 +0100

    Add Ruby on Rails 8 OpenTelemetry test app
    Add a Ruby app with OpenTelemetry that we can use for internal testing.

---
+++ ruby/rails8-sidekiq-collector/app/config/initializers/sidekiq.rb
@@ -0,0 +1,6 @@
+Sidekiq.configure_server do |config|
+  console_logger = ActiveSupport::Logger.new(STDOUT)
+  appsignal_logger = Appsignal::Logger.new("sidekiq")
+  config.logger = ActiveSupport::BroadcastLogger.new(console_logger, appsignal_logger)
+  config.logger.formatter = Sidekiq::Logger::Formatters::WithoutTimestamp.new
+end
