1e53e8239f305422354865c9d70e5453341f40c5
Noemi <45180344+unflxw@users.noreply.github.com>
2025-02-17 17:27:37 +0100

    Add slow and error endpoints
    Add `/slow` and `/error` endpoints, like in other test setups. The
    error does not seem to be reported to AppSignal.
    
    Added a root endpoint that lists the other endpoints.

--- java/spring-boot/app/spring-native/src/main/java/io/opentelemetry/example/graal/Controller.java
+++ java/spring-boot/app/spring-native/src/main/java/io/opentelemetry/example/graal/Controller.java
@@ -2,12 +2,31 @@ package io.opentelemetry.example.graal;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class Controller {
+  @GetMapping("/slow")
+  public String slow() throws InterruptedException {
+    Thread.sleep(3000);
+    return "Well, that took forever!";
+  }
 
-  @GetMapping("/ping")
-  public String ping() {
-    return "pong";
+  @GetMapping("/error")
+  public String error() {
+    throw new RuntimeException("Whoops!");
+  }
+
+  @GetMapping("/")
+  public String root() {
+    return "<html>" +
+           "<body>" +
+           "<h1>Java + Spring Boot test setup:</h1>" +
+           "<ul>" +
+           "<li><a href=\"/slow\">/slow</a></li>" +
+           "<li><a href=\"/error\">/error</a></li>" +
+           "</ul>" +
+           "</body>" +
+           "</html>";
   }
 }
