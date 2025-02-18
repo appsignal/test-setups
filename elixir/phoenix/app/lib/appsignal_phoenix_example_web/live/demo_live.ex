6d3edd7d43ce6b48fab6779e9708c10b6cc32d21
Tom de Bruijn <tom@tomdebruijn.com>
2025-02-18 10:38:34 +0100

    Add working live view example (#273)
    The live view demo didn't report any live view data, probably because of
    some optimization.
    
    Make the component interactable to report live view events.
    
    Disable the Phoenix top bar code because it threw an error.

--- elixir/phoenix/app/lib/appsignal_phoenix_example_web/live/demo_live.ex
+++ elixir/phoenix/app/lib/appsignal_phoenix_example_web/live/demo_live.ex
@@ -1,11 +1,47 @@
 defmodule AppsignalPhoenixExampleWeb.DemoLive do
   use AppsignalPhoenixExampleWeb, :live_view
 
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, :brightness, 50)}
+  end
+
+  def handle_params(unsigned_params, uri, socket) do
+    IO.puts("handle_params event")
+    IO.inspect([unsigned_params, uri, socket])
+    {:noreply, socket}
+  end
+
   def render(assigns) do
     ~H"""
-    Hello LiveView!
+    <h1>LiveView test light</h1>
+
+    <div class="light">
+      <div class="bar" style={"width: #{@brightness}%"}>
+        <%= @brightness %>%
+      </div>
+    </div>
+
+    <button phx-click="down">
+      Down
+    </button>
+
+    <button phx-click="up">
+      Up
+    </button>
 
     <.live_component module={AppsignalPhoenixExampleWeb.LiveComponent} id="component" />
     """
+  end
+
+  def handle_event("down", _, socket) do
+    brightness = socket.assigns.brightness - 10
+    socket = assign(socket, :brightness, brightness)
+    {:noreply, socket}
+  end
+
+  def handle_event("up", _, socket) do
+    brightness = socket.assigns.brightness + 10
+    socket = assign(socket, :brightness, brightness)
+    {:noreply, socket}
   end
 end
