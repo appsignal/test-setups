f392c56db95210a07a7f8e04a2ed4bb10f445dac
Noemi <45180344+unflxw@users.noreply.github.com>
2025-02-19 11:06:38 +0100

    Add agent-based Java + Spring Boot app
    Adds a Spring test setup that uses the Java OpenTelemetry "agent"
    instead of the so-called "native" Java OpenTelemetry setup.
    
    (This is not to be mistaken with the AppSignal agent -- both setups
    use the AppSignal collector, not the agent)

---
+++ java/spring-agent-collector/app/build.gradle.kts
@@ -0,0 +1,40 @@
+import org.springframework.boot.gradle.plugin.SpringBootPlugin
+
+plugins {
+    id("java")
+    id("org.springframework.boot") version "3.4.2"
+    id("org.graalvm.buildtools.native") version "0.10.5"
+    id("com.diffplug.spotless") version "7.0.2"
+}
+
+group = "io.opentelemetry"
+version = "0.1.0-SNAPSHOT"
+description = "OpenTelemetry Example for Spring native images"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(platform(SpringBootPlugin.BOM_COORDINATES))
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+    implementation("com.h2database:h2")
+}
+
+spotless {
+    java {
+        targetExclude("**/generated/**")
+        googleJavaFormat()
+    }
+}
+
+tasks.bootRun {
+    if (project.hasProperty("jvmargs")) {
+        jvmArgs = (project.property("jvmargs") as String).split("\\s+".toRegex())
+    }
+    if (project.hasProperty("args")) {
+        args = (project.property("args") as String).split("\\s+".toRegex())
+    }
+}
