48d2550bcf735a5191266a84349eb724dc43d9af
Jeff Kreeftmeijer <jeff@kreeft.me>
2025-02-19 16:38:34 +0100

    Add demo.php to ensure the configuration works

---
+++ php/laravel-collector/app/demo.php
@@ -0,0 +1,103 @@
+<?php
+
+use OpenTelemetry\API\Instrumentation\CachedInstrumentation;
+use OpenTelemetry\API\Trace\Span;
+use OpenTelemetry\API\Trace\StatusCode;
+use OpenTelemetry\Context\Context;
+
+use OpenTelemetry\API\Globals;
+use OpenTelemetry\API\Logs\EventLogger;
+use OpenTelemetry\API\Logs\LogRecord;
+use OpenTelemetry\API\Trace\Propagation\TraceContextPropagator;
+use OpenTelemetry\Contrib\Otlp\LogsExporter;
+use OpenTelemetry\Contrib\Otlp\MetricExporter;
+use OpenTelemetry\Contrib\Otlp\SpanExporter;
+use OpenTelemetry\Contrib\Otlp\OtlpHttpTransportFactory;
+use OpenTelemetry\SDK\Common\Attribute\Attributes;
+use OpenTelemetry\SDK\Common\Export\Stream\StreamTransportFactory;
+use OpenTelemetry\SDK\Logs\LoggerProvider;
+use OpenTelemetry\SDK\Logs\Processor\SimpleLogRecordProcessor;
+use OpenTelemetry\SDK\Metrics\MeterProvider;
+use OpenTelemetry\SDK\Metrics\MetricReader\ExportingReader;
+use OpenTelemetry\SDK\Resource\ResourceInfo;
+use OpenTelemetry\SDK\Resource\ResourceInfoFactory;
+use OpenTelemetry\SDK\Sdk;
+use OpenTelemetry\SDK\Trace\Sampler\AlwaysOnSampler;
+use OpenTelemetry\SDK\Trace\Sampler\ParentBased;
+use OpenTelemetry\SDK\Trace\SpanProcessor\SimpleSpanProcessor;
+use OpenTelemetry\SDK\Trace\TracerProvider;
+use OpenTelemetry\SemConv\ResourceAttributes;
+
+require 'vendor/autoload.php';
+
+/* The class to be instrumented */
+class DemoClass
+{
+    public function run(): void
+    {
+        echo 'Hello, world';
+    }
+}
+
+/* The auto-instrumentation code */
+OpenTelemetry\Instrumentation\hook(
+    class: DemoClass::class,
+    function: 'run',
+    pre: static function (DemoClass $demo, array $params, string $class, string $function, ?string $filename, ?int $lineno) {
+        static $instrumentation;
+        $instrumentation ??= new CachedInstrumentation('example');
+        $span = $instrumentation->tracer()->spanBuilder('democlass-run')->startSpan();
+        Context::storage()->attach($span->storeInContext(Context::getCurrent()));
+    },
+    post: static function (DemoClass $demo, array $params, $returnValue, ?Throwable $exception) {
+        $scope = Context::storage()->scope();
+        $scope->detach();
+        $span = Span::fromContext($scope->context());
+        if ($exception) {
+            $span->recordException($exception);
+            $span->setStatus(StatusCode::STATUS_ERROR);
+        }
+        $span->end();
+    }
+);
+
+$resource = ResourceInfoFactory::emptyResource()->merge(ResourceInfo::create(Attributes::create([
+    ResourceAttributes::SERVICE_NAMESPACE => 'demo',
+    ResourceAttributes::SERVICE_NAME => 'test-application',
+    ResourceAttributes::SERVICE_VERSION => '0.1',
+    "appsignal.config.name" => getenv("APPSIGNAL_APP_NAME"),
+    "appsignal.config.environment" => getenv("APPSIGNAL_APP_ENV"),
+    "appsignal.config.push_api_key" => getenv("APPSIGNAL_PUSH_API_KEY"),
+    "appsignal.config.revision" => "test-setups",
+    "appsignal.config.language_integration" => "php",
+    "host.name" => gethostname(),
+    "service.name" => "Laravel server",
+])));
+$transport = (new OtlpHttpTransportFactory())->create('http://appsignal-collector:8099/v1/traces', 'application/x-protobuf');
+$spanExporter = new SpanExporter($transport);
+
+$reader = new ExportingReader(new MetricExporter($transport));
+
+$meterProvider = MeterProvider::builder()
+    ->setResource($resource)
+    ->addReader($reader)
+    ->build();
+
+$tracerProvider = TracerProvider::builder()
+    ->addSpanProcessor(
+        new SimpleSpanProcessor($spanExporter)
+    )
+    ->setResource($resource)
+    ->setSampler(new ParentBased(new AlwaysOnSampler()))
+    ->build();
+
+Sdk::builder()
+    ->setTracerProvider($tracerProvider)
+    ->setMeterProvider($meterProvider)
+    ->setPropagator(TraceContextPropagator::getInstance())
+    ->setAutoShutdown(true)
+    ->buildAndRegisterGlobal();
+
+/* Run the instrumented code, which will generate a trace */
+$demo = new DemoClass();
+$demo->run();
