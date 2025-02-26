839bf47fc0a9a46927194d47709411c531230956
Tom de Bruijn <tom@tomdebruijn.com>
2025-02-26 09:33:05 +0100

    Add PHP Laravel slow and error routes
    Allow us to test the basic routes.

--- php/laravel-collector/app/routes/web.php
+++ php/laravel-collector/app/routes/web.php
@@ -6,6 +6,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/slow', function () {
+    sleep(3);
+    return view('slow');
+});
+
+Route::get('/error', function () {
+  throw new Exception('Uh oh!');
+});
+
 Route::get('/extension', function () {
     return extension_loaded("opentelemetry") ? "yes" : "no";
 });
