95c76cdaa13ecf17d63e491f2cce03a494218789
Shairyar Baig <shairyarbaig@gmail.com>
2025-03-05 19:15:32 +0500

    Add check-in to the task

--- ruby/single-task/app/app.rb
+++ ruby/single-task/app/app.rb
@@ -25,39 +25,18 @@ class Monitor
       puts "Monitor is doing stuff. Params: #{param1}, #{options}, #{foo}, #{error}"
       sleep 1
     end
-    Appsignal.increment_counter "test_counter", rand(1..3), "host" => "my-host"
-    Appsignal.set_gauge "test_gauge", rand(1..100), "host" => "my-host"
-    Appsignal.add_distribution_value "test_distribution", rand(1..100), "host" => "my-host"
-
-    raise CustomError, "Demo error" if error
   end
   appsignal_instrument_method :perform
 end
 
-20.times do |i|
-  # Test with monitor_transaction helper
+5.times do |i|
+  # Test with monitor helper
   begin
-    Appsignal.monitor_transaction("perform_job.monitor", :class => "MonitorTransaction", :method => "loop") do
-      Monitor.new.perform("foo", { :foo => i }, foo: i, error: i.even?)
+    Appsignal.monitor(:action => :MonitorTransaction) do
+      Appsignal::CheckIn.cron("testing") do
+        Monitor.new.perform("foo", { :foo => i }, foo: i, error: i.even?)
+      end
     end
-  rescue CustomError
-    # Do nothing, tracked by monitor_transaction
+    # Do nothing, tracked by monitor
   end
-
-  # Test using transaction class directly, like most AppSignal integrations do
-  begin
-    transaction = Appsignal::Transaction.create(
-      SecureRandom.uuid,
-      Appsignal::Transaction::HTTP_REQUEST,
-      Appsignal::Transaction::GenericRequest.new({})
-    )
-    Monitor.new.perform("foo", { :foo => i }, foo: i, error: i.even?)
-  rescue => error
-    transaction.set_error(error)
-  ensure
-    transaction.set_action_if_nil("MonitorCustomTransaction#loop")
-    Appsignal::Transaction.complete_current!
-  end
-
-  sleep 1
 end
