202c85058ea2ec98a51e8400e3b6af2d8ca11177
Noemi <45180344+unflxw@users.noreply.github.com>
2025-04-29 17:06:30 +0200

    Use custom Oban config prefix
    Use a custom Oban prefix to test support for the Oban config prefix
    in the Elixir integration.

--- elixir/plug-oban/app/priv/repo/migrations/20221128161801_add_oban_jobs_table.exs
+++ elixir/plug-oban/app/priv/repo/migrations/20221128161801_add_oban_jobs_table.exs
@@ -2,12 +2,12 @@ defmodule PlugOban.Repo.Migrations.AddObanJobsTable do
   use Ecto.Migration
 
   def up do
-    Oban.Migrations.up(version: 11)
+    Oban.Migrations.up(prefix: "some_prefix", version: 11)
   end
 
   # We specify `version: 1` in `down`, ensuring that we'll roll all the way back down if
   # necessary, regardless of which version we've migrated `up` to.
   def down do
-    Oban.Migrations.down(version: 1)
+    Oban.Migrations.down(prefix: "some_prefix", version: 1)
   end
 end
