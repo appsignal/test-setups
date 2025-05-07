c28047fe16df2e7094403f428599a20d3107365a
Tom de Bruijn <tom@tomdebruijn.com>
2025-05-07 10:07:26 +0200

    Add example tags to the PHP app
    Test tags quickly because they're already set in the test app.

--- php/laravel-collector/app/routes/web.php
+++ php/laravel-collector/app/routes/web.php
@@ -3,6 +3,19 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    $span = OpenTelemetry\API\Trace\Span::getCurrent();
+    $span->setAttribute("appsignal.tag.user_id", 123);
+    $span->setAttribute("appsignal.tag.my_tag_string", "tag value");
+    $span->setAttribute("appsignal.tag.my_tag_string_slice", ["abc", "def"]);
+    $span->SetAttribute("appsignal.tag.my_tag_bool_true", true);
+    $span->SetAttribute("appsignal.tag.my_tag_bool_false", false);
+    $span->SetAttribute("appsignal.tag.my_tag_bool_slice", [true, false]);
+    $span->SetAttribute("appsignal.tag.my_tag_float64", 12.34);
+    $span->SetAttribute("appsignal.tag.my_tag_float64_slice", [12.34, 56.78]);
+    $span->SetAttribute("appsignal.tag.my_tag_int", 1234);
+    $span->SetAttribute("appsignal.tag.my_tag_int_slice", [1234, 5678]);
+    $span->SetAttribute("appsignal.tag.my_tag_int64", 1234);
+    $span->SetAttribute("appsignal.tag.my_tag_int64_slice", [1234, 5678]);
     return view('welcome');
 });
 
