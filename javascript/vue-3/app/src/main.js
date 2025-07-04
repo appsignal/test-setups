b618592fd9fba6652dd9dc39bbab2b89342fbcc4
Tom de Bruijn <tom@tomdebruijn.com>
2025-07-04 22:54:21 +0200

    Update Vue 3 app to newer version
    Create a new Vue 3 app because the old one didn't want to install on an
    older Node.js version.

--- javascript/vue-3/app/src/main.js
+++ javascript/vue-3/app/src/main.js
@@ -1,13 +1,18 @@
+import './assets/main.css'
+
 import { createApp } from 'vue'
 import App from './App.vue'
-
-import Appsignal from "@appsignal/javascript"
+import router from './router'
+import { appsignal } from "./appsignal";
 import { errorHandler } from "@appsignal/vue"
-
-const appsignal = new Appsignal({
-  key: process.env.VUE_APP_APPSIGNAL_FRONTEND_KEY
-})
 
 const app = createApp(App)
 app.config.errorHandler = errorHandler(appsignal, app)
+// app.config.errorHandler = (err, instance, info) => {
+//   console.log("!!! error", err, instance, info)
+//   // handle error, e.g. report to a service
+// }
+
+app.use(router)
+
 app.mount('#app')
---
+++ javascript/vue-3/app/src/views/AboutView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <button @click="throwError">TEST ERROR</button>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+
+<script>
+export default {
+  name: 'ErrorButton',
+  methods: {
+    throwError() {
+      throw new Error('Vue 3 TEST ERROR')
+    }
+  }
+}
+</script>
