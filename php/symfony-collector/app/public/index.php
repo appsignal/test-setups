85018e20202cc665ce7e8770cdb8c7643c7e971f
Tom de Bruijn <tom@tomdebruijn.com>
2025-07-15 14:54:38 +0200

    Add OpenTelemetry to Symfony app
    Follow the OpenTelemetry install instructions on the AppSignal docs.
    Update the appsignal.php file to load the env vars from the .env file
    like we do in the PHP Laravel app.

--- php/symfony-collector/app/public/index.php
+++ php/symfony-collector/app/public/index.php
@@ -4,6 +4,9 @@ use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
+// Load AppSignal OpenTelemetry configuration
+require_once dirname(__DIR__).'/appsignal.php';
+
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
