d7e4cf910ffdc16a579389004bd2382b763bfb30
Tom de Bruijn <tom@tomdebruijn.com>
2025-08-19 09:44:39 +0200

    Add custom span to Koa example app
    Make it more clear what part of the trace is from what app, in case they
    get mixed up.

--- nodejs/koa-microservices/app1/src/app.ts
+++ nodejs/koa-microservices/app1/src/app.ts
@@ -2,7 +2,8 @@ import Koa from "koa"
 import Router from "@koa/router"
 import bodyParser from "koa-bodyparser"
 import fetch from "node-fetch"
-import { setTag } from "@appsignal/nodejs"
+import { setTag, setCategory } from "@appsignal/nodejs"
+import { trace } from "@opentelemetry/api"
 
 const app = new Koa()
 const router = new Router()
@@ -25,7 +26,15 @@ router.get("/slow", async (ctx: any) => {
 })
 
 router.get("/remote/app2", async (ctx: any) => {
+  const tracer = trace.getTracer("my-interesting-app");
   setTag("original_app_id", "app1");
+  await tracer.startActiveSpan("event.custom_app1", async (span) => {
+    setCategory("event.custom_app1");
+    setTag("original_trace_id", span.spanContext().traceId);
+    console.log("!!! Original trace ID:", span.spanContext().traceId);
+    await new Promise((resolve) => setTimeout(resolve, 100))
+    span.end()
+  })
   try {
     const response = await fetch("http://app2:4002/incoming/app2")
     const data = await response.text()
@@ -37,7 +46,14 @@ router.get("/remote/app2", async (ctx: any) => {
 })
 
 router.get("/incoming/app1", async (ctx: any) => {
+  const tracer = trace.getTracer("my-interesting-app");
   setTag("remote_app_id", "app1");
+  await tracer.startActiveSpan("event.custom_app1", async (span) => {
+    setCategory("event.custom_app1");
+    setTag("remote_trace_id", span.spanContext().traceId);
+    console.log("!!! Remote trace ID:", span.spanContext().traceId);
+    span.end()
+  })
 
   ctx.body = "Request received by app1!"
 })
--- nodejs/koa-microservices/app2/src/app.ts
+++ nodejs/koa-microservices/app2/src/app.ts
@@ -2,7 +2,8 @@ import Koa from "koa"
 import Router from "@koa/router"
 import bodyParser from "koa-bodyparser"
 import fetch from "node-fetch"
-import { setTag } from "@appsignal/nodejs"
+import { setTag, setCategory } from "@appsignal/nodejs"
+import { trace } from "@opentelemetry/api"
 
 const app = new Koa()
 const router = new Router()
@@ -25,7 +26,14 @@ router.get("/slow", async (ctx: any) => {
 })
 
 router.get("/remote/app1", async (ctx: any) => {
+  const tracer = trace.getTracer("my-interesting-app");
   setTag("original_app_id", "app2");
+  tracer.startActiveSpan("event.custom_app2", (span) => {
+    setCategory("event.custom_app2");
+    setTag("original_trace_id", span.spanContext().traceId);
+    console.log("!!! Original trace ID:", span.spanContext().traceId);
+    span.end()
+  })
   try {
     const response = await fetch("http://app1:4001/incoming/app1")
     const data = await response.text()
@@ -37,7 +45,14 @@ router.get("/remote/app1", async (ctx: any) => {
 })
 
 router.get("/incoming/app2", async (ctx: any) => {
+  const tracer = trace.getTracer("my-interesting-app");
   setTag("remote_app_id", "app2");
+  tracer.startActiveSpan("event.custom_app2", (span) => {
+    setCategory("event.custom_app2");
+    setTag("remote_trace_id", span.spanContext().traceId);
+    console.log("!!! Remote trace ID:", span.spanContext().traceId);
+    span.end()
+  })
 
   ctx.body = "Request received by app2!"
 })
