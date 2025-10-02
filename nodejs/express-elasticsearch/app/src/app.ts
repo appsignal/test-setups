0b4c94dd56b69d44be722b89a02bb4a245efb8ff
Martin Lipskis <lipskis@users.noreply.github.com>
2025-10-02 17:03:39 +0300

    Add a hard-coded HTML list of routes
    Use setup function to initialize index on app startup and handle missing
    index.
    
    Remove `winston`, all its dependencies, logger setup and any calls to
    it.
    
    Add `WebWorker` to `compilerOptions.lib` in `tsconfig.json` to fix
    missing `StreamPipeOptions` in `apache/arrow-js`.

--- nodejs/express-elasticsearch/app/src/app.ts
+++ nodejs/express-elasticsearch/app/src/app.ts
@@ -1,54 +1,8 @@
 import express, { Response, Request } from "express"
 import { Client } from "@elastic/elasticsearch"
-import { setTag, setCustomData, expressErrorHandler, WinstonTransport } from "@appsignal/nodejs"
-import { trace } from "@opentelemetry/api"
+import { expressErrorHandler } from "@appsignal/nodejs"
 import cookieParser from "cookie-parser"
-import winston from "winston"
-
-const { combine, printf } = winston.format
-
-const customLogLevels = {
-	levels: {
-		trace: 8,
-		debug: 7,
-		info: 6,
-		notice: 5,
-		warn: 4,
-		error: 3,
-		crit: 2,
-		alert: 1,
-		emerg: 0,
-	},
-}
-
-const customformat = printf(({ level, message, label, timestamp }) => {
-	return `${timestamp} [${label}] ${level}: ${message}`
-})
-
-const logger = winston.createLogger({
-	level: "trace",
-	levels: customLogLevels.levels,
-	format: combine(customformat),
-	transports: [
-		new WinstonTransport({ group: "app" }),
-	],
-})
-
-const client = new Client({
-	node: process.env.ELASTIC_URL,
-})
-
-const port = process.env.PORT
-
-const app = express()
-app.use(express.urlencoded({ extended: true }))
-app.use(cookieParser())
-app.use(express.json())
-
-app.get("/", (_req: any, res: any) => {
-	logger.info("Home path")
-	res.sendStatus(200)
-})
+import { IndicesCreateResponse, SearchHit } from "@elastic/elasticsearch/lib/api/types"
 
 type Product = {
 	name: string
@@ -59,41 +13,66 @@ type Product = {
 
 const PRODUCT_INDEX = "products"
 
+const client = new Client({
+	node: process.env.ELASTIC_URL,
+})
+
+// this is async, should be fine for demo
+createProductIndex()
+
+const port = process.env.PORT
+
+const app = express()
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(express.json())
+
+app.get("/", (_req: any, res: any) => {
+	client.search<Product>({
+		index: PRODUCT_INDEX,
+		query: {
+			match_all: {}
+		},
+	})
+		.then(r => {
+			const docs = r.hits.hits
+			res.send(getHtmlLinks(docs))
+		})
+		.catch(err => {
+			if (err.meta.statusCode === 404) {
+				res.send(getInitialHtml())
+				return
+			}
+
+			res.status(500).send(err)
+		})
+})
+
+// cluster info
 app.get("/cluster-info", (_req: Request, res: Response) => {
 	client.info()
 		.then((r) => {
 			res.json({ message: "Got cluster info", info: r })
-		}).catch((err) => console.log(err))
+		}).catch((err) => res.status(500).send(err))
 })
 
 // create index
 app.post("/index", (_req: Request, res: Response) => {
-	client.indices
-		.create({
-			index: PRODUCT_INDEX,
-			mappings: {
-				properties: {
-					name: { type: "text" },
-					price: { type: "integer" },
-					category: { type: "keyword" },
-					featured: { type: "boolean" },
-				}
-			}
-		})
+	createProductIndex()
 		.then((r) => {
 			res.send(r)
 		})
-		.catch((err) => console.log(err))
+		.catch((err) => res.status(500).send(err))
 })
 
 // delete index
 app.delete("/index", (_req: Request, res: Response) => {
 	client.indices
 		.delete({ index: PRODUCT_INDEX })
-		.then(() => {
-			res.sendStatus(200)
+		.then((r) => {
+			res.send(r)
 		})
-		.catch((err) => console.log(err))
+		.catch((err) => res.status(500).send(err))
 })
 
 // index document
@@ -112,7 +91,7 @@ app.post("/products", (req: Request, res: Response) => {
 		.then((r) => {
 			res.json({ message: "Product indexed", id: r._id })
 		})
-		.catch((err) => console.log(err))
+		.catch((err) => res.status(500).send(err))
 })
 
 // bulk index
@@ -132,7 +111,7 @@ app.post("/products/bulk", (_req: Request, res: Response) => {
 				"errors": r.errors,
 			})
 		})
-		.catch((err) => console.log(err))
+		.catch((err) => res.status(500).send(err))
 })
 
 // search
@@ -157,7 +136,7 @@ app.get("/products/search", (req: Request, res: Response) => {
 				})),
 			})
 		})
-		.catch((e) => console.log(e))
+		.catch((err) => res.status(500).send(err))
 })
 
 // get document
@@ -170,7 +149,7 @@ app.get("/products/:product_id", (req: Request, res: Response) => {
 		.then((r) => {
 			res.json({ result: r })
 		})
-		.catch((err) => console.log(err))
+		.catch((err) => res.status(500).send(err))
 })
 
 // update document
@@ -179,12 +158,12 @@ app.put("/products/:product_id", (req: Request, res: Response) => {
 		.update<Product>({
 			index: PRODUCT_INDEX,
 			id: req.params.product_id,
-			doc: req.body
+			doc: { name: req.body?.name ?? getRandomName() },
 		})
 		.then((r) => {
 			res.json({ message: 'Product updated', result: r })
 		})
-		.catch((err) => console.log(err))
+		.catch((err) => res.status(500).send(err))
 })
 
 // delete document
@@ -197,19 +176,7 @@ app.delete("/products/:product_id", (req: Request, res: Response) => {
 		.then((r) => {
 			res.json({ message: 'Product deleted', result: r })
 		})
-		.catch((e) => console.log(e))
-})
-
-
-app.get("/custom", (_req: Request, res: Response) => {
-	logger.info("custom route")
-	setCustomData({ custom: "data" })
-
-	trace.getTracer("custom").startActiveSpan("Custom span", span => {
-		setTag("custom", "tag")
-		span.end()
-	})
-	res.send("200 OK")
+		.catch((err) => res.status(500).send(err))
 })
 
 app.use(expressErrorHandler())
@@ -221,4 +188,77 @@ app.listen(port, () => {
 function getRandomName(): string {
 	const id = Math.round(Math.random() * 100)
 	return `Product-${id}`
+}
+
+function createProductIndex(): Promise<IndicesCreateResponse | string> {
+	return client.indices.exists({ index: PRODUCT_INDEX })
+		.then(exists => {
+			if (!exists) {
+				return client.indices
+					.create({
+						index: PRODUCT_INDEX,
+						mappings: {
+							properties: {
+								name: { type: "text" },
+								price: { type: "integer" },
+								category: { type: "keyword" },
+								featured: { type: "boolean" },
+							}
+						}
+					})
+			} else {
+				return new Promise((resolve) => {
+					resolve("Index exists")
+				})
+			}
+		})
+}
+
+function mapToHtmlList(documents: Array<SearchHit<Product>>, callback: (doc: SearchHit<Product>) => string): string {
+	let list = '<ul>'
+	for (const doc of documents) {
+		list += `<li>${callback(doc)}</li>`
+	}
+	list += '</ul>'
+
+	return list
+}
+
+function getInitialHtml() {
+	return `
+		<h1>Elasticsearch (Express) example </h1>
+		<p>"${PRODUCT_INDEX}" index is missing</p>
+		<form method="POST" action="/index"><button>Create index</button></form>
+	`
+}
+
+function getHtmlLinks(docs: Array<SearchHit<Product>>): string {
+	const productGetList = mapToHtmlList(docs, (doc) => `<a href="/products/${doc._id}">${doc._source?.name}</a>`)
+	const productUpdateList = mapToHtmlList(docs, (doc) => `<form onsubmit="event.preventDefault();fetch('/products/${doc._id}', {method: 'PUT', headers: {'Content-Type': 'application/json'}, body: JSON.stringify({name: this.name.value})}).then(r => r.text()).then(res => { const blob = new Blob([res], {type: 'application/json'}); window.location.href = window.URL.createObjectURL(blob); })"><input name="name" value="${doc._source?.name}"><button>Update</button></form>`)
+	const productDeleteList = mapToHtmlList(docs, (doc) => `<button onclick="fetch('/products/${doc._id}', {method: 'DELETE'}).then(r => r.text()).then(res => { const blob = new Blob([res], {type: 'application/json'}); window.location.href = window.URL.createObjectURL(blob); })">${doc._source?.name}</button>`)
+
+	return `
+		<h1>Elasticsearch (Express) example</h1>
+		<p>POST, PUT and DELETE requests will return json responses in the same tab. Refresh "/" page to see updates.</p>
+		<p>Try these routes:</p>
+		<ul>
+			<li><a href="/cluster-info" target="_blank">Cluster info</a></span>
+			<li><h2>Indices</h2>
+				<ul>
+					<li><form method="POST" action="/index"><button>Create "${PRODUCT_INDEX}" index</button></form></li>
+					<li><button onclick="fetch('/index', {method: 'DELETE'}).then(r => r.text()).then(res => { const blob = new Blob([res], {type: 'application/json'}); window.location.href = window.URL.createObjectURL(blob); })">Delete "${PRODUCT_INDEX}" index</button></li>
+				</ul>
+			</li>
+			<li><h2>Documents</h2>
+				<ul>
+					<li><h3>Create</h3><form method="POST" action="/products"><input name="name" placeholder="Product name..." type="text"><button>Create</button></form></li>
+					<li><h3>Bulk create</h3><form method="POST" action="/products/bulk"><button>Bulk create</button></form></li>
+					<li><h3>Search</h3><form method="GET" action="/products/search"><input name="query" type="text" placeholder="Search by product name..."><button>Search</button></form></li>
+					<li><h3>Get</h3>${productGetList}</li>
+					<li><h3>Update</h3>${productUpdateList}</li>
+					<li><h3>Delete</h3>${productDeleteList}</li>
+				</ul>
+			</li>
+		</ul>
+	`
 }
