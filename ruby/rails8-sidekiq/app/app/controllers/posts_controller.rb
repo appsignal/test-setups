741de8b95b14f58f2520d6a6ff302b0c5ee31fa9
Tom de Bruijn <tom@tomdebruijn.com>
2025-11-11 11:41:50 +0100

    Add routes with queries to Rails 8 Sidekiq app
    Make testing with queries in logs easier.

---
+++ ruby/rails8-sidekiq/app/app/controllers/posts_controller.rb
@@ -0,0 +1,52 @@
+class PostsController < ApplicationController
+  owner :examples
+
+  def index
+    @posts = Post.order(created_at: :desc)
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to posts_path, notice: "Post was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to post_path(@post), notice: "Post was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path, notice: "Post was successfully deleted."
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body, :author)
+  end
+end
--- ruby/rails8-sidekiq/app/db/schema.rb
+++ ruby/rails8-sidekiq/app/db/schema.rb
@@ -10,5 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_11_102016) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.string "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
