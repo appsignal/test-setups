70da4f550b19a90484896cf06108fc2134e42e52
Tom de Bruijn <tom@tomdebruijn.com>
2025-11-11 18:20:08 +0100

    Merge pull request #344 from appsignal/rails-crud
    Add routes with queries to Rails 8 Sidekiq app

--- ruby/rails8-sidekiq/app/config/routes.rb
+++ ruby/rails8-sidekiq/app/config/routes.rb
@@ -16,4 +16,6 @@ Rails.application.routes.draw do
   get "/error_cause", to: "examples#error_cause"
   get "/error_reporter", to: "examples#error_reporter"
   get "/custom_error", to: "examples#custom_error"
+
+  resources :posts
 end
