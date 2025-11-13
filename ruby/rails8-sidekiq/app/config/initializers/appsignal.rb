f29c7dea0f0acabce9d0bd9da884cb9bed2d3434
Jeff Kreeftmeijer <jeff@kreeft.me>
2025-11-13 17:20:39 +0100

    Implement start and stop actions in Appsignal::EventSubscriber

--- ruby/rails8-sidekiq/app/config/initializers/appsignal.rb
+++ ruby/rails8-sidekiq/app/config/initializers/appsignal.rb
@@ -1,6 +1,22 @@
 class Appsignal::EventSubscriber
   def emit(event)
-    pp event
+    name = "#{event[:name].split(".").reverse.join(".")}_event"
+
+    case event[:name]
+    when "action_controller.request_started", "action_view.render_start", "action_view.render_start"
+      start(name, event)
+    when "action_view.render_template", "action_view.render_layout", "action_controller.request_completed"
+      stop(name, event)
+    end
+  end
+
+  def start(name, event)
+    Appsignal::Transaction.current.start_event()
+  end
+
+  def stop(name, event)
+    title, body, body_format = Appsignal::EventFormatter.format(name, event[:payload])
+    Appsignal::Transaction.current.finish_event(name, title, body, body_format)
   end
 end
 
