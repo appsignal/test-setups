2f05fcf5d0bf3403b42e341efe83c014d06a6432
Shairyar Baig <shairyarbaig@gmail.com>
2025-12-24 12:31:44 +0500

    Testing ignore actions in rails 6.1.7.10

--- ruby/rails6-mongo/app/config/routes.rb
+++ ruby/rails6-mongo/app/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get "/slow", to: "tests#slow"
   get "/error", to: "tests#error"
+  get 'healthy' => 'health_check#show', as: :health_check
 
   resources :posts, :except => %i[edit update destroy]
 end
